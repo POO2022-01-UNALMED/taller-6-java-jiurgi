ca93011cdba78c41acf237237b58a6e7f096f7cb
Jimena Uribe Giraldo <jiuribeg@unal.edu.co>
2022-05-10 23:56:57 -0500

    .

---
+++ src/main/java/Fabricante.java
@@ -0,0 +1,65 @@
+package vehiculos;
+
+import java.util.ArrayList;
+
+public class Fabricante {
+
+	private String nombre;
+	private Pais pais;
+	
+	private int ventas;
+	private static ArrayList<Fabricante> fabricantes = new ArrayList<Fabricante>();
+
+	public Fabricante (String nombre, Pais pais){
+		this.nombre = nombre;
+		this.pais = pais;
+		
+		fabricantes.add(this);
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+	public void setVentas(int ventas) {
+		this.ventas = ventas;
+	}
+
+	public int getVentas() {
+		return ventas;
+	}
+	
+	public void agregarVenta() {
+		ventas++;
+		pais.agregarVenta();
+	}
+	
+	public static Fabricante fabricaMayorVentas () {
+		Fabricante masVendedor = null;
+		int mayoresVentas = 0;
+
+		for (int i = 0; i < fabricantes.size(); i++) {
+			if (fabricantes.get(i).ventas > mayoresVentas) {
+				masVendedor = null;
+				masVendedor = fabricantes.get(i);
+				mayoresVentas = fabricantes.get(i).ventas;
+			}
+	    }
+
+		return masVendedor;
+	}
+
+}
---
+++ src/main/java/Pais.java
@@ -0,0 +1,57 @@
+package vehiculos;
+
+import java.util.ArrayList;
+
+public class Pais {
+	
+	private String nombre;
+	
+	private int ventas;
+	private static ArrayList<Pais> paises = new ArrayList<Pais>();
+
+	public Pais (String nombre){
+		
+		this.nombre = nombre;
+		
+		paises.add(this);
+	
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setVentas(int ventas) {
+		this.ventas = ventas;
+	}
+
+	public int getVentas() {
+		return ventas;
+	}
+	
+	public void agregarVenta() {
+		ventas++;
+	}
+	
+	public static Pais paisMasVendedor () {
+		
+		Pais masVendedor = null;
+		int mayoresVentas = 0;
+
+		for (int i = 0; i < paises.size(); i++) {
+			if (paises.get(i).ventas > mayoresVentas) {
+				masVendedor = null;
+				masVendedor = paises.get(i);
+				mayoresVentas = paises.get(i).ventas;
+			}
+	    }
+
+		return masVendedor;
+		
+	}
+
+}
